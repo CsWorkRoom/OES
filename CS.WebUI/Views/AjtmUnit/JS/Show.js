03858420b93c1f24055b824ea1f4be7591d9e952
zjl <zjl@qq.com>
2021-07-05 19:49:08 +0800

    update

---
+++ CS.WebUI/Views/AjtmUnit/JS/Show.js
@@ -0,0 +1,20 @@
+﻿layui.use(['element', 'form', 'layer', 'jquery', 'laydate'], function () {
+    var element = layui.element, form = layui.form, layer = layui.layer, $ = layui.$, laydate = layui.laydate;
+
+    $('.layui-tab-title').on('click', function (t) {
+        var unitid = $("#UnitId").val();
+        var e = t.target;
+        switch (e.getAttribute("lay-id")) {
+            case "tabAsUnit":
+                $("#iframeAsUnit").attr("src", "");
+                break;
+            case "AsDetail":
+                $("#iframeAsDetail").attr("src", "");
+                break;
+            case "AsLeader":
+                $("#iframeAsLeader").attr("src", "");
+                break;
+            default: break;
+        }
+    });
+});
