01585c2ce2637b2c9e14f78ff1fb2f695072787d
zjl <zjl@qq.com>
2021-07-27 23:06:11 +0800

    update

---
+++ CS.WebUI/Controllers/AJTM/AjtmFileManageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CS.WebUI.Controllers.AJTM
+{
+    public class AjtmFileManageController : Controller
+    {
+        // GET: AjtmFileManage
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
